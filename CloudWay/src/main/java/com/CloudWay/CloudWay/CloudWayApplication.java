9873fa1fcaa2c86da91f46c20f8447bb377d34be
RajeshAyyavu <141205735+RajeshAyyavu@users.noreply.github.com>
2024-07-22 14:03:44 +0530

    Add files via upload

---
+++ CloudWay/src/main/java/com/CloudWay/CloudWay/CloudWayApplication.java
@@ -0,0 +1,13 @@
+package com.CloudWay.CloudWay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudWayApplication.class, args);
+	}
+
+}
